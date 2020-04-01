ceb1fc88ded4b96256d0b8ca39c0b1d12e0d92af
Emil Jan <anoldwebsite@gmail.com>
2020-04-01 12:40:44 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/* By using a class component instead of a function, we get access to state inside our class, whic is a js object with properties. */
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      string: "this text"
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{this.state.string}</p>
+          <button
+            onClick={() => {
+              this.state.string === "this text"
+                ? this.setState({ string: "THAT TEXT" })
+                : this.setState({ string: "this text" });
+            }}
+          >
+            click me to change the text above dynamically
+          </button>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React from Scratch
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root') /* This dev element with the id 'root' in the file index.html is replaced by our component <App /> by the method render in the ReactDOM that we have imported in this module. */
 );
 
 // If you want your app to work offline and load faster, you can change
