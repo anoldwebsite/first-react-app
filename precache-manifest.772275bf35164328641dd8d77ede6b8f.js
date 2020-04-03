cade286095cf21a5bf8c062920f9c8447fd5017f
Emil Jan <anoldwebsite@gmail.com>
2020-04-03 14:24:20 +0100

    Updates

---
+++ precache-manifest.772275bf35164328641dd8d77ede6b8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd55f27fbc502202a0f8b91eadaeb109",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "06218923cfc9e118b130",
+    "url": "/first-react-app/static/css/main.40e869a2.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/first-react-app/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/first-react-app/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06218923cfc9e118b130",
+    "url": "/first-react-app/static/js/main.2eb877f4.chunk.js"
+  },
+  {
+    "revision": "d573cf7118a500f7985e",
+    "url": "/first-react-app/static/js/runtime-main.d74cba17.js"
+  }
+]);
